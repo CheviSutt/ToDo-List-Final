bccea1a2cd7c3e7f08c0f34b27058c6a312b3d3a
CheviSutt <35573303+CheviSutt@users.noreply.github.com>
2018-04-02 11:22:59 -0600

    Add files via upload
    I got the inputs working and now i need to solve my css bug

--- toDo.js
+++ toDo.js
@@ -1,3 +1,66 @@
+
+
+$(".bluebtn").click(function(event){
+    let myval = $("#myInput").val();
+    // if(event.which === 13){
+    $(".listContainer").append("<div class='todolist'>" + myval +
+            " <input id='" + myval + "' onkeyup='addtheItem(this, this.value, event)'placeholder='Add Item Press Enter' type='text'><div><a class=\"OrgBtn\" onclick=\"testItem()\">Add Item</a><div class='itembox'></div></div>");
+        $('#myInput').val("");
+        // }
+
+});
+
+function addtheItem(incid, myvalue, event){
+     if(event.keyCode === 13){
+        $(incid).parent().find(".itembox").append(" <div class=\"row\">\n" +
+            "<div class=\"rowleft\">\n" +
+            "<div class=\"iconspot fa\"></div>\n" +
+            "<div class=\"label\">" + myvalue +"</div>\n" +
+            "</div>\n" +
+            "<div class=\"rowright\">\n" +
+            "<button onclick=\"markComplete(this)\">Mark Complete</button>\n" +
+            "<div class=\"fa fa-trash\" onclick=\"removeRow(this)\"></div>\n" +
+            "</div></div>");
+        $(incid).val("");
+     }
+}
+
+addtheItem()
+
+
+//add a row
+function markComplete(myelement){
+    let mydad = $(myelement).parent().parent();
+    $(mydad).toggleClass("complete");
+
+    $(mydad).find(".iconspot").toggleClass("fa-check");
+}
+
+function removeRow(theeElement) {
+    let grandpa = $(theeElement).parent().parent();
+    /*$(grandpa).remove();*/
+//remove all edit marks for function
+    $(grandpa).slideUp('1000', function () {
+        //fires after slide up is done
+        $(grandpa).remove();
+    })
+
+}
+
+//orange button test
+function testItem() {
+    document.getElementById("test").innerHTML = "<div class=\"row\">\n" +
+        "<div class=\"rowleft\">\n" +
+        "<div class=\"iconspot fa\"></div>\n" +
+        " <div class=\"label\">Feed the Dog</div>\n" +
+        "</div>\n" +
+        " <div class=\"rowright\">\n" +
+        "<button onclick=\"markComplete(this)\">Mark Complete</button>\n" +
+        "<div class=\"fa fa-trash\" onclick=\"removeRow(this)\"></div>\n" +
+        "</div>\n" +
+        "</div>";
+}
+
 // Create a new list item when clicking the "Add list" button W3
 function newElement() {
     var li = document.createElement("li");
@@ -23,60 +86,4 @@ function newElement() {
             div.style.display = "none";
         }
     }
-}
-
-//add empty div todolist
-/*function addList(){
-   let list = document.createElement("div");
-   document.getElementById("newList").append(listLabel);
-}*/
-
-$(".bluebtn").click(function(event){
-    let myval = $("#myInput").val();
-    // if(event.which === 13){
-    $(".listContainer").append("<div class='todolist'>" + myval +
-            " <input id='" + myval + "' onclick='addtheItem(this, this.value, event)'placeholder='Add A Item' type='text'> <a class=\"OrgBtn\" onclick=\"addtheItem()\">Add Item</a><div class='itembox'></div></div>");
-        $('#myInput').val("");
-        // }
-
-});
-
-function addtheItem(incid, myvalue, event){
-    // if(event.keyCode === 13){
-        $(incid).parent().find(".itembox").append(" <div class=\"row\">\n" +
-            "<div class=\"rowleft\">\n" +
-            "<div class=\"iconspot fa\"></div>\n" +
-            "<div class=\"label\">" + myvalue +"</div>\n" +
-            "</div>\n" +
-            " <div class=\"rowright\">\n" +
-            "<button onclick=\"markComplete(this)\">Mark Complete</button>\n" +
-            "<div class=\"fa fa-trash\" onclick=\"removeRow(this)\"></div>\n" +
-            "</div></div>");
-        $(incid).val("");
-    // }
-}
-
-addtheItem()
-
-$(".OrgBtn").click(function(event) {
-
-})
-
-
-function markComplete(myelement){
-    let mydad = $(myelement).parent().parent();
-    $(mydad).toggleClass("complete");
-
-    $(mydad).find(".iconspot").toggleClass("fa-check");
-}
-
-function removeRow(theeElement) {
-    let grandpa = $(theeElement).parent().parent();
-    /*$(grandpa).remove();*/
-//remove all edit marks for function
-    $(grandpa).slideUp('1000', function () {
-        //fires after slide up is done
-        $(grandpa).remove();
-    })
-
 }
